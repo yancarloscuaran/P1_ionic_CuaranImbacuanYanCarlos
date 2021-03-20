f67f7d3472dbecbcddd5ca5a17c91b962e8eb89b
yancarloscuaran <yancarloscuaranimbacuan@gmail.com>
2021-03-20 11:37:24 -0500

    Agregando calculadora al home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +8,72 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  constante = 'numero';
+  operador = '+';
+  decimal = false;
+  decimales = 0;
+  pantalla = 0;
+  memoria = 0;
+
+  Numero(num: number) {
+    switch (this.constante) {
+      case 'numero':
+        if (this.decimal) {
+          this.decimales++;
+          this.pantalla = this.pantalla + num * Math.pow(10, -this.decimales);
+        } else {
+          this.pantalla = this.pantalla * 10 + num;
+        }
+        break;
+      case 'operador':
+        this.pantalla = num;
+        this.constante = 'numero';
+        break;
+      case 'resultado':
+        this.memoria = 0;
+        this.pantalla = num;
+        this.constante = 'numero';
+    }
+  }
+
+  Operador(opr: string) {
+    this.calcular();
+    this.operador = opr;
+    this.memoria = this.pantalla;
+    this.constante = 'operador';
+  }
+
+  calcular() {
+    this.pantalla = eval('' + this.memoria + this.operador + '(' + this.pantalla + ')');
+    this.memoria = 0;
+    this.constante = 'resultado';
+    this.operador = '+';
+    this.decimal = false;
+    this.decimales = 0;
+  }
+
+  Borrartodo() {
+    this.pantalla = 0;
+    this.memoria = 0;
+    this.constante = 'numero';
+    this.operador = '+';
+    this.decimal = false;
+    this.decimales = 0;
+  }
+
+  Borrarnumero() {
+    this.pantalla = 0;
+    this.constante = 'numero';
+    this.decimal = false;
+    this.decimales = 0;
+  }
+
+  Decimalnum() {
+    this.decimal = true;
+  }
+
+  Signo() {
+    this.pantalla = this.pantalla * -1;
+  }
 
 }
